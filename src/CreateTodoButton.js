3ea53b64185827aa23fd2e0f4bdb56d9c2dea72c
Irving mtz <al2173074687@azc.uam.mx>
2024-12-18 05:46:27 -0600

    uso de onClick y onChange para la interaccion

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,12 @@ import './CreateTodoButton.css'
 import masIcon from './icons/masIcon.svg'
 function CreateTodoButton(){
     return(
-        <button>
+        <button 
+            onClick={(event) => { 
+            console.log("Le diste click al boton de agregar");
+            console.log(event);
+            console.log(event.target);
+            }}>
             <img src={masIcon} alt='imagen de flecha mas' />
         </button>
     );
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,11 @@
 import './TodoSearch.css';
 function TodoSearch(){
     return(
-        <input placeholder="Cortar Cebolla"></input>
+        <input placeholder="Cortar Cebolla"
+        onChange={(event)=> {
+            console.log(event.target.value);
+        }}
+        ></input>
         );
     }
 
