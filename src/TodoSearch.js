547cb6a10910aafe6578ed71e536424740cb7c25
Irving mtz <al2173074687@azc.uam.mx>
2024-12-18 06:34:20 -0600

    uso de una variable usando useState para guardar o manipular la informacion

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './TodoSearch.css';
+
 function TodoSearch(){
+    const [searchValue, setSearchValue] = React.useState('');
+    console.log('Palabra:  '+searchValue)
     return(
         <input placeholder="Cortar Cebolla"
+        value={searchValue}
         onChange={(event)=> {
-            console.log(event.target.value);
+            setSearchValue(event.target.value);
         }}
         ></input>
         );
