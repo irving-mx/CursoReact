6bae4322ca0209806a0123475d495060ef15c991
Irving mtz <al2173074687@azc.uam.mx>
2025-01-11 23:46:51 -0600

    uso de localStorage en react, cargarlo manualmente con el uso de JSON.stringify y en local traerlo de regreso con JSON.parse

--- src/App.js
+++ src/App.js
@@ -5,29 +5,48 @@ import {TodoItem} from './TodoItem'
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
-const defaultTodos = [
-  {text: 'Cortar Cebolla', completed: true},
-  {text: 'Tomar el Curso de Intro a React.js', completed: false},
-  {text: 'Llorar con la llorina', completed: false},
-  {text: 'Escuchar la cumbia de la UAM', completed: false},
-  {text: 'Echarte un perreo', completed: false},
-  {text: 'Ir a las posadas', completed: true},
-  {text: 'Comer mucha comida', completed: true}
-]
+// const defaultTodos = [
+//   {text: 'Cortar Cebolla', completed: true},
+//   {text: 'Tomar el Curso de Intro a React.js', completed: false},
+//   {text: 'Llorar con la llorina', completed: false},
+//   {text: 'Escuchar la cumbia de la UAM', completed: false},
+//   {text: 'Echarte un perreo', completed: false},
+//   {text: 'Ir a las posadas', completed: true},
+//   {text: 'Comer mucha comida', completed: true}
+// ]
+
+// localStorage.setItem("defaultTodos_V1", defaultTodos);
+
+// localStorage.removeItem("defaultTodos_V1");
+
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem("defaultTodos_V1");
+  let parseTodos;
+  if(localStorageTodos){
+    parseTodos = JSON.parse(localStorageTodos);
+  }else{
+    parseTodos = [];  
+  }
+
+
+  const [todos, setTodos] = React.useState(parseTodos);
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter((todo)=>todo.completed).length;
   const totalTodos = todos.length;
   const searchedTodos = todos.filter(search => search.text.toLowerCase().includes(searchValue.toLowerCase()));
+
+  const saveTodos = (newTodos) => {
+    localStorage.setItem("defaultTodos_V1",JSON.stringify(newTodos));
+    setTodos(newTodos);
+  }
 
   const selectTodo = (text) => {
     const newTodos = [...todos];
     console.log("Seleccione: " + text)
     const indexTodos = newTodos.findIndex( todo => todo.text == text); 
     newTodos[indexTodos].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
@@ -35,7 +54,7 @@ function App() {
     console.log("Seleccione: " + text)
     const indexTodos = newTodos.findIndex( todo => todo.text == text); 
     newTodos.splice(indexTodos, 1)
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
 
